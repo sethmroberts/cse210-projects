4541d58712fb7ba22af36a515afb4268bdd0d656
Seth Roberts <sethsoar315@gmail.com>
2025-04-24 08:25:13 -0600

    added my name to README

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // this is a program to compute the area of a circle
+
+        // get the radius of a circle from the user
+        Console.Write("please enter the radius: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        // compute the area of a circle
+        double area = Math.PI * radius * radius;
+
+        // display the area for the user to see.
+        Console.WriteLine($"Area of the circle: {area}");
     }
 }
