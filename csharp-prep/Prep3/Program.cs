e9c038da3719a0e68f74729f3af5680a2f8140b6
Seth Roberts <sethsoar315@gmail.com>
2025-04-30 12:52:30 -0600

    prep 4, 5, 6

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Formats.Asn1;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        int guess = 0;
+        string input = "";
+        List<int> guesses = new List<int>();
+        Console.WriteLine("Please guess the magic number.");
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            input = Console.ReadLine();
+            guess = int.Parse(input);
+            guesses.Add(guess);
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine($"Well done! You guessed the magic number in {guesses.Count} guesses!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        while (number != 0)
+        {
+            
+            Console.Write("Enter a list of numbers, type 0 when finished.");
+            number = int.Parse(Console.ReadLine());
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        }
+
+        /* Compute sum, average, and max. */
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+
+        /* Print results to the user. */
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is {max}");
     }
+
 }
