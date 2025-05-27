f8341c1fd8b90e2abe996e9cf619876dbf95fa23
Seth Roberts <sethsoar315@gmail.com>
2025-05-27 13:17:37 -0600

    week05 learning

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+/// <summary>
+/// Represents a fraction with a numerator and denominator
+/// This is more accurate than storing a double.
+/// </summary>
+
+public class Fraction
+{
+    private int _numer;
+    private int _denom;
+
+    public Fraction()
+    {
+        _numer = 1;
+        _denom = 1;
+    }
+
+    public Fraction(int whole)
+    {
+        _numer = whole;
+        _denom = 1;
+    }
+
+    public Fraction(int numer, int denom)
+    {
+        _numer = numer;
+        _denom = denom;
+    }
+
+    public string GetFractionString()
+    {
+        string result = $"{_numer} / {_denom}";
+        return result;
+
+    }
+
+    public double GetDecimalValue()
+    {
+        double result = (double)_numer / (double)_denom;
+        return result;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new();
+        Console.WriteLine(f1.GetFractionString());
+        Fraction f2 = new();
     }
 }
